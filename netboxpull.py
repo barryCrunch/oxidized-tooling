6363526b46fd71f2052adf33e188d97b5cd113ec
barryCrunch <mbarry@packetdriving.com>
2019-02-14 19:59:07 +0000

    added groups to routerdb output

--- netboxpull.py
+++ netboxpull.py
@@ -10,22 +10,24 @@ def validateDevices(devices):
     for i in devices:
         if i.platform != None and i.primary_ip4 != None:
             validDevices.append(i)
-
     return validDevices
 
 def writeDB(devices):
     with open('router.db', 'w') as file:
         for i in devices:
+            if 'noenable' in i.tags:
+                group = 'noenable'
+            else:
+                group = 'enable'
             d = {}
             d['name'] = i.name
             d['ip'] = i.primary_ip4
             d['model'] = i.platform.slug
             user = os.environ['USERNAME']
             passwd = os.environ['PASSWORD']
-            enable = os.environ['ENABLE']
             file.write(f"{i.name.split(' ')[0]}:"
                         f"{i.primary_ip4.address.split('/')[0]}"
-                        f":{i.platform.slug}:{user}:{passwd}:{enable}\n")
+                        f":{i.platform.slug}:{user}:{passwd}:{group}\n")
 
 if __name__ == '__main__':
     nb = pynetbox.api(
